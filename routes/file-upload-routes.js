7b653263ecd1e084bf8684854360889d7bffaade
mohamad <msiysinyuy@gmail.com>
2022-03-03 16:00:53 +0100

    update on andriod route

--- routes/file-upload-routes.js
+++ routes/file-upload-routes.js
@@ -3,11 +3,12 @@
 const express = require('express')
 const {upload} = require('../helpers/filehelper')
 const { protect,isAdmin } = require('../middleware/auth')
-const {singleFileUpload,multipleFileUpload,getallSingleFiles,getallMultipleFiles} = require('../controllers/fileuploaderController')
+const {singleFileUpload,multipleFileUpload,multipleFileUpload2,getallSingleFiles,getallMultipleFiles} = require('../controllers/fileuploaderController')
 const router = express.Router();
 
 router.post('/singleFile',upload.single('file'),singleFileUpload);
 router.post('/multipleFiles',upload.array('files'),multipleFileUpload);
+router.post('/multipleFiles2',multipleFileUpload2);
 router.get('/getallSingleFiles',protect,isAdmin,getallSingleFiles)
 router.get('/getallMultipleFiles',getallMultipleFiles)
 
